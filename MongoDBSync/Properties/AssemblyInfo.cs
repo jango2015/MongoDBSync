db97cdc35bca2abc3182f3df028558f8ff97241c
Josh McCullough <joshua.mccullough@gmail.com>
2015-07-12 15:10:23 -0400

    Created project structure.

---
+++ MongoDBSync/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("MongoDBSync")]
+[assembly: AssemblyTitle("MongoDBSync")]
+[assembly: AssemblyDescription("Synchronous extensions for the asynchronous MongoDB .NET driver.")]
+[assembly: AssemblyCompany("Quantum Concepts")]
+[assembly: AssemblyCopyright("Copyright © Quantum Concepts")]
+[assembly: AssemblyTrademark("Copyright © Quantum Concepts")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
